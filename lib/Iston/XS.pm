187e4f6646515a7f90abaf1b1ee2b3a9ca173e4d
basiliscos <dmol@gmx.com>
2014-11-15 19:12:48 -0800

    More correctly create module distribution

---
+++ lib/Iston/XS.pm
@@ -0,0 +1,5 @@
+package Iston::XS;
+
+our $VERSION  = '0.01';
+
+1;
--- lib/Iston/XS/Utils.pm
+++ lib/Iston/XS/Utils.pm
@@ -1,14 +1,14 @@
 package Iston::XS::Utils;
 
 use 5.16.0;
+use Iston::XS;
 
 require Exporter;
 our @ISA = qw(Exporter);
 
 our @EXPORT_OK = qw/find_uniq_pixels find_matching_pixels/;
 
-our $VERSION  = '0.01';
 require XSLoader;
-XSLoader::load("Iston::XS::Utils", $VERSION);
+XSLoader::load("Iston::XS::Utils", $Iston::XS::VERSION);
 
 1;
